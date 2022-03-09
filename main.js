3ad2cd15c1f954b1dda07418ee008d4259cade75
Marshall Mazagwu Jr <hello@planetpublic.com>
2022-03-08 21:44:26 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let count = 0
+let counter = document.getElementById("new")
+console.log(counter)
+function increment() {
+    count = count + 1
+    counter.innerText = count
+    console.log("Test")
+}
+
