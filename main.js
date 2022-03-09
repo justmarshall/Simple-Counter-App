35562b14535ec697980c10b9066844ce54a0da78
Marshall Mazagwu Jr <hello@planetpublic.com>
2022-03-08 21:45:36 -0600

    JS update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let count = 0
-let counter = document.getElementById("new")
+let counter = document.getElementById("counterElement")
 console.log(counter)
 function increment() {
     count = count + 1
